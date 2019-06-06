2eec94b01967aad557355a0c69c91bdbcf189353
Dev Laptop temp <john.abraham@hronboard.me>
2019-06-06 23:05:29 +1000

    playing with mongoose connections

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as mongoose from "mongoose";
 
-
+const MONGODB_URL = "mongodb://localhost:27017/kaputi";
 const app = express();
 
 app.disable("x-powered-by");
@@ -10,12 +10,10 @@ app.disable("x-powered-by");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "1mb" }));
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("mongodb connection open!")
-});
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true}).then(
+  () => console.log("mongodb connection open!"),
+  (error) => {throw new Error(error)}
+);
 
 app.get("/", (req, res)=> res.send('Hello World!'));
 
